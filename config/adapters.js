4a06f9b775064dff4f0c40d001c0dd38739984fe
Daniel Grant <danieljohngrant@gmail.com>
2013-11-09 23:16:11 +0000

    Use disk

--- config/adapters.js
+++ config/adapters.js
@@ -5,13 +5,13 @@ module.exports.adapters = {
 
 	// If you leave the adapter config unspecified
 	// in a model definition, 'default' will be used.
-	//'default': 'disk',
-	'default': 'mongo',
+	'default': 'disk',
+	// 'default': 'mongo',
 
-	mongo: {
-    module   : 'sails-mongo',
-    url      : process.env.MONGOLAB_URI || 'mongodb://localhost:27017/sails'
-  },
+	// mongo: {
+ //    module   : 'sails-mongo',
+ //    url      : process.env.MONGOLAB_URI || 'mongodb://localhost:27017/sails'
+ //  },
 
 	// In-memory adapter for DEVELOPMENT ONLY
 	// (data is NOT preserved when the server shuts down)
