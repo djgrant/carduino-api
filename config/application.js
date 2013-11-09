27a6da4bd62024d046ef2dfa3838f49da424a201
Daniel Grant <danieljohngrant@gmail.com>
2013-11-09 23:39:28 +0000

    Application title

--- config/application.js
+++ config/application.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	// Name of the application (used as default <title>)
-	appName: "Sails Application",
+	appName: "Carduino",
 
 	// Port this Sails application will live on
 	port: process.env.PORT || 1337,
